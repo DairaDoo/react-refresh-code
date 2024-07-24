4bb977e4716fa20f7ae98a6244d17ee6ec6471d0
DairanDoo <your_email@example.com>
2024-07-24 17:09:03 -0400

    Codigo actual.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Post from "./components/Post";
+
+
+// you can always return one single element on react. You must have on root JSX element.
+function App() {
+  return (
+  <main>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </main>
+  );
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+    return (
+    <div>
+        <p>{}</p>
+        <p>{}</p>
+    </div>
+    );
+}
+
+export default Post;
