95c609ef42812afd6ee2d702162caafa66794104
DairanDoo <your_email@example.com>
2024-07-24 17:11:49 -0400

    Usando random para elegir un nombre y reutilizando el mismo componente.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,14 @@
+const names = ['Dairan', 'Samir'];
+
 function Post() {
+
+    // choose a random name any time the page is up.
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
     return (
     <div>
-        <p>{}</p>
-        <p>{}</p>
+         <p>{chosenName}</p> {/* Between those curly braces we can put any javascript expresion*/}
+        <p>React.js is an amazing framework.</p>
     </div>
     );
 }
