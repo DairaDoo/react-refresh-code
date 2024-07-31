7ae9a37630a3efa660f49e49fe4e3fa7800f8e7c
DairanDoo <your_email@example.com>
2024-07-31 02:59:09 -0400

    Uso de rutas utilizando react-router-dom para dividir el contenido en rutas y añadiendo un loading state para retroalimentación.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import { useState, useEffect } from "react";
 function PostsList({ isPosting, onStopPosting }) {
 
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false); // estado para carga mientras se llaman los post (para retroalimentación).
 
   useState(() => {
     async function fetchPosts() {
+      setIsFetching(true); // cargando...
       const response = await fetch('http://localhost:8080/posts');
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false); // terminado...
     }
 
     fetchPosts();
@@ -37,15 +40,20 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
         </Modal>
       )} 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
         {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}  {/* se mapea en cada post y crea uno por cada elemento en el array. */}
       </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{textAlign: 'center', color: 'white'}}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"; // ReactRouter nos ayuda a crear rutas distintas en nuestro proyecto.
+import App from "./App";
+import NewPost from "./components/NewPost";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {path: '/', element: <App />}, // <our-domain>
+  {path: '/create-post', element: <NewPost />} // <we call create post componente in this route>
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
