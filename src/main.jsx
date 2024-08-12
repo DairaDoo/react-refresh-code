df8815f0b7b151f752891954368bf387f1eb97e2
DairanDoo <your_email@example.com>
2024-08-12 15:20:39 -0400

    Loader añadido en index.html arreglado

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom"; // ReactRouter nos ayuda a crear rutas distintas en nuestro proyecto.
+import { RouterProvider, createBrowserRouter } from "react-router-dom"; 
 import Posts, { loader as postsLoader } from "./routes/Posts";
 import NewPost, { action as newPostAction } from "./routes/NewPost";
-import PostDetails, {loader as postDetailsLoader} from "./routes/PostDetails";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 import RootLayout from "./routes/RootLayout";
 import Loading from "./components/Loading";
 
+// Configura las rutas de tu aplicación
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,15 +21,21 @@ const router = createBrowserRouter([
         errorElement: <Loading />,
         children: [
           { path: "/create-post", element: <NewPost />, action: newPostAction },
-          { path: "/:postId", element: <PostDetails />, loader: postDetailsLoader}
+          { path: "/:postId", element: <PostDetails />, loader: postDetailsLoader }
         ],
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Renderiza la aplicación y oculta el loader inicial una vez montada
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// Oculta el loader inicial una vez que React esté montado
+document.getElementById('initial-loader').style.display = 'none';
